59db887afbcf9a36dd7bf706091a8f5daef4eb1c
sergejjarosch <sergejjarosch@gmail.com>
2024-03-01 09:45:49 +0100

    add counter

--- index.js
+++
@@ -1,7 +0,0 @@
-let fullName = "BroSergej";
-let age = 25;
-let student = false;
-
-document.getElementById("p1").textContent = `Your name is ${fullName}`;
-document.getElementById("p2").textContent = `You are ${age} years old`;
-document.getElementById("p3").textContent = `Enrolled: ${student}`;
