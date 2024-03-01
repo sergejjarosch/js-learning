cfd4feb8e88d4df51e8b9b8b80aa8c305ea7def9
sergejjarosch <sergejjarosch@gmail.com>
2024-03-01 16:14:16 +0100

    add slice

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
+const fullName = "Sergej      Jarosch";
 
-
-
-// let age = 21;
-// let message = age >= 18 ? "You're an adult": "You're a minor";
-
-// console.log(message);
-
-
-// let time = 13;
-// let greeting = time < 12 ? "Good Morning!" : "Good afternoon!";
-// console.log(greeting)
-
-// let isStudent = false;
-// let message = isStudent ? "You are a student" : "You are not a student";
-// console.log(message);
-
-let purchaseAmount = 29;
-let discound = purchaseAmount >= 100 ? 10 : 0;
-console.log(`Your total is $${purchaseAmount - purchaseAmount * (discound / 100)}`);
+let firstName = fullName.slice(0, fullName.indexOf(" "));
+let lastName = fullName.slice(fullName.indexOf(" ")+1);
+lastName = lastName.replaceAll(" ", "");
+console.log(firstName);
+console.log(lastName);
