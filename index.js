522a8d1420ff4727eafa78e742921e215979a794
sergejjarosch <sergejjarosch@gmail.com>
2024-03-01 11:53:06 +0100

    add if else

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
-let count = 0;
 
-increaseBtn.onclick = function(){
-    count++;
-    countLabel.textContent = count;
+
+let age = 15;
+let hasLicense = true
+
+if (age >= 16) {
+    console.log("You are old enough to drive");
+    if (hasLicense) {
+        console.log("You have your license!");
+    } else {
+        console.log("You do not have your license yet");
+    }
+    
+} else{
+    console.log("You must be 16+ to have a license");
 }
-
-decreaseBtn.onclick = function(){
-    count--;
-    countLabel.textContent = count;
-}
-
-resetBtn.onclick = function(){
-    count = 0;
-    countLabel.textContent = count;
-}
-
