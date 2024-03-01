a460ccc72a4663e1a04cf065a6f7bdbba57ee6e0
sergejjarosch <sergejjarosch@gmail.com>
2024-03-01 13:58:07 +0100

    add els if

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 
 
-let age = 15;
-let hasLicense = true
 
-if (age >= 16) {
-    console.log("You are old enough to drive");
-    if (hasLicense) {
-        console.log("You have your license!");
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const PayPalBtn = document.getElementById("PayPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function(){
+    if (myCheckBox.checked) {
+        subResult.textContent ="You are subscribed! I like you!";
     } else {
-        console.log("You do not have your license yet");
+        subResult.textContent = "You are NOT subscribed";
     }
-    
-} else{
-    console.log("You must be 16+ to have a license");
+    if (visaBtn.checked) {
+        paymentResult.textContent ="You are paying with Visa";
+    }
+    else if (masterCardBtn.checked) {
+        paymentResult.textContent = "You are paying with Master Card";
+    } 
+    else if (PayPalBtn.checked) {
+        paymentResult.textContent = "You are paying with PayPal";
+    }
+    else {
+        paymentResult.textContent = "You must select a payment type";
+    }
 }
+
+
+
+
