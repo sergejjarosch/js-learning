ecdf6a22b437b7f76f4d4d3b66105c6d2f180f86
sergejjarosch <sergejjarosch@gmail.com>
2024-03-04 15:55:53 +0100

    add an array

--- script/index.js
+++ script/index.js
@@ -1,7 +1,20 @@
-let fruit = ["apple", "orange", "banana", "coconut"];
-fruit.sort().reverse();
 
-for (let fruits of fruit){
-    console.log(fruits);
+
+function sum(...numbers){
+    let result =0;
+    for(let number of numbers){
+        result += number;
+    }
+    return result;
 }
 
+function getAverage(...numbers){
+    let result =0;
+    for(let number of numbers){
+        result += number;
+    }
+    return result / numbers.length;
+}
+
+const total = getAverage(75,100,85,50);
+console.log(total);
