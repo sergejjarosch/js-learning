6430b3550ccfceb07ad5940c811abd91a4934ebb
sergejjarosch <sergejjarosch@gmail.com>
2024-03-04 10:00:01 +0100

    add do while and while do

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+let loggedIn = false;
+let username;
+let password;
 
-
-let username = window.prompt("Enter your Username:");
-username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
-console.log(username);
+do{
+    username = window.prompt("Enter your username:");
+    password = window.prompt("Enter your password:");
+    if (username === "myUsername" && password === "password"){
+        loggedIn = true;
+        console.log("You are logged in!");
+    } else {
+        console.log("Invalid credentials! Please try again.");
+    }
+} while (!loggedIn)
