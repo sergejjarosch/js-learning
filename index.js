fea8799ff3cd631bd39a51319ec4fe9d77c7ff6a
sergejjarosch <sergejjarosch@gmail.com>
2024-03-04 09:12:33 +0100

    add combined operators

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 
 
-let username = window.prompt("Enter your USername:");
-
-username = username.trim();
-let letter = username.charAt(0);
-letter = letter.toUpperCase();
-
-let extraChars = username.slice(1);
-extraChars = extraChars.toLowerCase();
-username = letter + extraChars;
-
+let username = window.prompt("Enter your Username:");
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
 console.log(username);
-
