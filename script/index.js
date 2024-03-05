1cefbe050e3ee913b3adfac9af5fe64e8a857bb4
sergejjarosch <sergejjarosch@gmail.com>
2024-03-05 16:06:15 +0100

    arrow function

--- script/index.js
+++ script/index.js
@@ -4,4 +4,8 @@
 const numbers = [1,2,3,4,5,6];
 const squares = numbers.map( (element) => Math.pow(element, 2));
 const cubes = numbers.map( (element) => Math.pow(element, 3));
-console.log(cubes);
+const evenNums = numbers.filter((element) => element % 2 === 0);
+const oddNums = numbers.filter((element) => element % 2 !== 0);
+const total = numbers.reduce((accumulate, element) => accumulate + element);
+
+console.log(total);
