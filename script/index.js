e1fbe7cd67ec9ebaea8d2f7177411746aeb40d07
sergejjarosch <sergejjarosch@gmail.com>
2024-03-05 16:31:49 +0100

    add changes

--- script/index.js
+++ script/index.js
@@ -1,11 +1,13 @@
 
 
 
-const numbers = [1,2,3,4,5,6];
-const squares = numbers.map( (element) => Math.pow(element, 2));
-const cubes = numbers.map( (element) => Math.pow(element, 3));
-const evenNums = numbers.filter((element) => element % 2 === 0);
-const oddNums = numbers.filter((element) => element % 2 !== 0);
-const total = numbers.reduce((accumulate, element) => accumulate + element);
+const person = {
+    firstName: "Spongebob",
+    lastName: "Squarepants",
+    age: 30,
+    isEmployed: true,
 
-console.log(total);
+}
+
+console.log(person.firstName);
+console.log(person.lastName);
