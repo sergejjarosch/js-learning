c2d48a5f1b03b43c6da533f471b690eb24968a03
sergejjarosch <sergejjarosch@gmail.com>
2024-03-05 14:11:34 +0100

    add filter - reduce min an max

--- script/index.js
+++ script/index.js
@@ -1,8 +1,19 @@
 
 
-const word = ["apple", "orange", "banana", "kiwi", "pomegranate" , "coconut"];
-const shortWords = word.filter(getShortWords);
-console.log(shortWords);
-function  getShortWords(element){
-    return element.length <= 6;
+
+
+const grades = [75, 50, 90, 80, 65, 95];
+
+const maximum = grades.reduce(getMax);
+const minimum = grades.reduce(getMin);
+
+console.log(maximum);
+console.log(minimum);
+
+function getMax(accumulator, element){
+    return Math.max(accumulator, element);
+}
+
+function getMin(accumulator, element){
+    return Math.min(accumulator, element);
 }
