82028301b8c211a67c83be3dd7b94cab56ed2a43
sergejjarosch <sergejjarosch@gmail.com>
2024-03-06 09:19:44 +0100

    add this method

--- script/index.js
+++ script/index.js
@@ -1,24 +1,11 @@
 
 
-
 const person1 = {
-    firstName: "Spongebob",
-    lastName: "Squarepants",
-    age: 30,
-    isEmployed: true,
-    sayHello: function(){console.log("Hi! I am Spongebob!")},
-    eat: () => {console.log("I am eating a Krabby Patty")},
+    name: "Spongebob",
+    favFood: "Hamburgers",
+    seyHallo:function(){console.log(`Hi! I am ${this.name}!`)},
+    eat: function(){console.log(`${this.name} is eating ${this.favFood}`)}
 }
 
-const person2 = {
-    firstName: "Patrick",
-    lastName: "Star",
-    age: 42,
-    isEmployed: false,
-    sayHello: () => {console.log("Hey, I'm Patrick...")},
-    eat: () => {console.log("I am eating roast beef")},
-}
-
-person1.sayHello();
-person2.sayHello();
+person1.seyHallo();
 person1.eat();
