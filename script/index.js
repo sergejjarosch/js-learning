04e655625cb1db96eca5508216110c0a3957663c
sergejjarosch <sergejjarosch@gmail.com>
2024-03-06 11:15:13 +0100

    add static

--- script/index.js
+++ script/index.js
@@ -7,10 +7,18 @@ class User{
         this.username = username;
         User.userCount++;
     }
+    static getUserCount(){
+        console.log(`There are ${this.userCount} users`);
+    }
+
+    sayHello(){
+        console.log(`Hello, my username is ${this.username}`);
+    }
 }
 
 const user1 = new User("Spongebob");
 const user2 = new User("Patrick");
 const user3 = new User("Sandy");
 
-console.log(User.userCount);
+user1.sayHello();
+User.getUserCount();
