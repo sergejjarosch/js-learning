26be9c15dc79db44fed977a1ccf48f91399a0fbe
sergejjarosch <sergejjarosch@gmail.com>
2024-03-06 15:28:58 +0100

    Distrukturisierung

--- script/index.js
+++ script/index.js
@@ -1,59 +1,24 @@
 
 
+//---- Distructure in Funktion Parameters --------
 
-class Person{
-
-    constructor(firstName, lastName, age){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
-
-    set firstName(newFirstName){
-        if(typeof newFirstName === "string" && newFirstName.length >0 ){
-            this._firstName = newFirstName;
-        }
-        else {
-            console.error("First name musst be a non-empty string ")
-        }
-    }
-    set lastName(newLastName){
-        if(typeof newLastName === "string" && newLastName.length >0 ){
-            this._lastName = newLastName;
-        }
-        else {
-            console.error("Last name musst be a non-empty string "); 
-        }
-    }
-
-    set age(newAge){
-        if (typeof newAge === "number" && newAge >= 0 ) {
-            this._age = newAge;
-        }
-        else {
-            console.error("Age must be a non-negative number  "); 
-        }
-    }
-    get firstName(){
-        return this._firstName;
-    }
-    get lastName(){
-        return this._lastName;
-    }
-    get fullName(){
-        return this._firstName + " " + this._lastName;
-    }
-
-    get age(){
-        return this._age;
-    }
-    
+function displayPerson({firstName, lastName, age, job = "Unemployed"}){
+    console.log(`name: ${firstName} ${lastName}`);
+    console.log(`age: ${age}`);
+    console.log(`job: ${job}`);
 }
 
+const person1 = {
+    firstName: "Spongebob",
+    lastName: "Squarepants",
+    age: 30,
+    job: "Fry Cook",
+}
 
-const person = new Person("Spongebob", "Squarepants", 30);
-console.log(person.firstName);
-console.log(person.lastName);
-console.log(person.fullName);
-console.log(person.age);
+const person2  = {
+    firstName: "Patrick",
+    lastName: "Star",
+    age: 34, 
+} 
 
+displayPerson(person2);
