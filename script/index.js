0424a6aa301bcae58ad0e9545197b787a85edf30
sergejjarosch <sergejjarosch@gmail.com>
2024-03-06 09:02:37 +0100

    add objects

--- script/index.js
+++ script/index.js
@@ -1,13 +1,24 @@
 
 
 
-const person = {
+const person1 = {
     firstName: "Spongebob",
     lastName: "Squarepants",
     age: 30,
     isEmployed: true,
-
+    sayHello: function(){console.log("Hi! I am Spongebob!")},
+    eat: () => {console.log("I am eating a Krabby Patty")},
 }
 
-console.log(person.firstName);
-console.log(person.lastName);
+const person2 = {
+    firstName: "Patrick",
+    lastName: "Star",
+    age: 42,
+    isEmployed: false,
+    sayHello: () => {console.log("Hey, I'm Patrick...")},
+    eat: () => {console.log("I am eating roast beef")},
+}
+
+person1.sayHello();
+person2.sayHello();
+person1.eat();
