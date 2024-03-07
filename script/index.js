9ef224bda620f88a911965255cd08fffc8cb3a73
sergejjarosch <sergejjarosch@gmail.com>
2024-03-07 10:25:38 +0100

    add filter

--- script/index.js
+++ script/index.js
@@ -2,8 +2,15 @@ const fruits = [{name: "apple", color: "red", calories:95},
                 {name: "orange", color: "orange", calories: 45},
                 {name: "banana", color: "yellow", calories: 105},
                 {name: "coconut", color: "white", calories: 159},
-                {name: "piniapple", color: "yelow", calories:37}];
+                {name: "piniapple", color: "yellow", calories:37}];
 
 fruits.push({name: "grapes", color: "purple", calories: 62});
 
-console.log(fruits);
+const yelowFruits = fruits.filter(fruit => fruit.color === "yellow");
+const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+const highCalFruits = fruits.filter(fruit => fruit.calories > 100);
+
+console.log(yelowFruits);
+console.log(lowCalFruits);
+console.log(highCalFruits);
+
