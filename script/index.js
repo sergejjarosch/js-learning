32d0e3af32e8808bfd7314fe30b84c11360e2834
sergejjarosch <sergejjarosch@gmail.com>
2024-03-07 11:31:40 +0100

    add sort

--- script/index.js
+++ script/index.js
@@ -2,19 +2,10 @@ const fruits = [{name: "apple", color: "red", calories:95},
                 {name: "orange", color: "orange", calories: 45},
                 {name: "banana", color: "yellow", calories: 105},
                 {name: "coconut", color: "white", calories: 159},
-                {name: "piniapple", color: "yellow", calories:37}];
-
-fruits.push({name: "grapes", color: "purple", calories: 62});
-
-
-const maxFruits = fruits.reduce((max, fruit) => 
-                                fruit.calories > max.calories ?
-                                fruit : max );
-
-const minFruits = fruits.reduce((min, fruit) => 
-                                fruit.calories < min.calories ?
-                                fruit : min );
-console.log(maxFruits);
-console.log(minFruits);
+                {name: "piniapple", color: "yelow", calories:37},
+                {name: "grapes", color: "purple", calories: 62}];
 
 
+
+fruits.sort((a,b) => a.calories - b.calories);
+console.log(fruits)
